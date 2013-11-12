33992dd48657c3ef931cf4ea78169c3902c94443
Jason Sims <jason@noesissoftware.com>
2013-11-12 08:20:28 -0600

    Added start route

--- app/controllers/database_cleaner/remote_api/database_cleans_controller.rb
+++ app/controllers/database_cleaner/remote_api/database_cleans_controller.rb
@@ -2,9 +2,14 @@ module DatabaseCleaner
   module RemoteApi
     class DatabaseCleansController < DatabaseCleaner::RemoteApi::ApplicationController
 
+      def create
+        DatabaseCleaner.start
+        render json: {status: 'started'}
+      end
+
       def show
         DatabaseCleaner.clean
-        render json: {}
+        render json: {status: 'cleaned'}
       end
 
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 DatabaseCleaner::RemoteApi::Engine.routes.draw do
   match 'clean' => 'database_cleans#show', via: [:get, :post], defaults: { format: :json }
+  match 'start' => 'database_cleans#create', via: [:get, :post], defaults: { format: :json }
 end
